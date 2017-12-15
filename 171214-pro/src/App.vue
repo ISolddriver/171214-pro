0c360bfd75db5896e1faea4ca810fc64e4d6f7d3
ISolddriver <yhex7111584@163.com>
2017-12-15 10:36:58 +0800

    hello world

--- 171214-pro/src/App.vue
+++ 171214-pro/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-   
-    <router-view/>
+    <div class="con" @click="handleClick">
+        {{message}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      message: 'hello world'
+    }
+  },
+  methods: {
+    handleClick () {
+      this.message = 'bye World'
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .con {
+    color: red;
+  }
 </style>
